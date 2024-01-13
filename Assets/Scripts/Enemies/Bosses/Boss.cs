8672365c9b5f23adacaa05dcb0a96e637f053873
kidkidkidkidkid1 <mozambaniac@gmail.com>
2024-01-13 00:31:00 -0800

    Continue work on first boss
    this is harder than i thought. more work needs to be done to the Projectile class and Boss collision logic. Likely soon will add a new class Weapon which the player uses to shoot projectiles, and will require some recoding to both PlayerController and Projectile. still need to figure out why camera doesnt lerp in build. all this and more moved my progress from about 90% base game functionality to maybe 60%

--- Assets/Scripts/Enemies/Bosses/Boss.cs
+++ Assets/Scripts/Enemies/Bosses/Boss.cs
@@ -9,6 +9,7 @@ public abstract class Boss : MonoBehaviour {
     protected GameObject player;
     protected Rigidbody2D rb;
     [SerializeField] private float onDieMinLaunchAngle, onDieMaxLaunchAngle;
+    [SerializeField] protected GameObject sprite;
     protected abstract void Initialize();
 
     // Start is called before the first frame update
@@ -25,20 +26,33 @@ public abstract class Boss : MonoBehaviour {
 
     protected void healthCheck() {
         if (health <= 0) {
-            StartCoroutine(die());
+            die();
         }
     }
 
-    protected IEnumerator die() {//this doesnt work, unsure why
-        print("dead");
+    protected void die() {  //this doesnt work, unsure why
         Vector2 launchVector = new Vector2(
                Mathf.Cos(Random.Range(onDieMinLaunchAngle, onDieMaxLaunchAngle)), 0);
         rb.velocity = new Vector2(0, 0);
-        rb.AddForce(launchVector * 5 * Random.Range(0.5f, 2.5f), ForceMode2D.Impulse);   //a lot of this could be combined into one big function but idk
-        rb.AddForce(Vector2.up * 5 * 2f, ForceMode2D.Impulse);
-        //rb.isKinematic = true;
-        yield return new WaitForSeconds(6);
-        Destroy(gameObject);
+        rb.AddForce(launchVector * 5 * Random.Range(0.5f, 2.5f));
+        rb.AddForce(Vector2.up * 20);
+        StartCoroutine(fakeGravity());
+        //rb.excludeLayers = (1 << 3) | (1 << 6);
+        StartCoroutine(queueForDeletion());
     }
 
+    private IEnumerator fakeGravity() {
+        rb.gravityScale = 0f;
+        float m = rb.mass;
+        float a = -Physics.gravity.magnitude;
+        rb.totalForce = new Vector2(0, 0);
+        while (true) {
+            rb.totalForce = new Vector3(0, m * a, 0);
+            yield return null;
+        }
+    }
+    private IEnumerator queueForDeletion() {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
 }
--- Assets/Scripts/Enemies/Bosses/TheCirculator.cs
+++ Assets/Scripts/Enemies/Bosses/TheCirculator.cs
@@ -1,49 +1,147 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TheCirculator : Boss {
     [SerializeField] private int rotationSpeed;
-    private GameObject turret;
+    [SerializeField] private GameObject turret;
     private Vector3 direction;
     private Vector3 direction2D;
-    protected List<GameObject> weakpoints = new List<GameObject>();
+    [SerializeField] private List<GameObject> weakpoints = new List<GameObject>();
     protected List<Collider2D> weakpointColliders = new List<Collider2D>();
+    private bool shouldRotate;
+    private bool shouldPointTurret;
+    private float curActionTime;
+    private bool hasPlayedSpawnTaunt;
+    private float vMovementBound = 19.5f;
+
+    [SerializeField] private Projectile projectile;
 
     // Start is called before the first frame update
     override protected void Initialize() {
-        for (int i = 0; i < transform.GetChild(0).childCount; i++) {
-            GameObject currentObject = transform.GetChild(0).GetChild(i).gameObject;
-            weakpoints.Add(currentObject);
-            weakpointColliders.Add(currentObject.GetComponent<Collider2D>());
-            print(weakpoints[i]);
+        foreach (GameObject weakpoint in weakpoints) {
+            weakpointColliders.Add(((GameObject)weakpoint).GetComponent<Collider2D>());
+            print(weakpoint);
         }
-        resetWeakpoints();
+        hasPlayedSpawnTaunt = false;
     }
 
     // Update is called once per frame
     void Update() {
         healthCheck();
-        transform.Rotate(new Vector3(0, 0, 1), rotationSpeed * Time.deltaTime);
-
-        turret = gameObject.transform.GetChild(1).GetChild(0).gameObject;
-        direction = player.transform.position - transform.position;
-        direction2D = Vector3.ProjectOnPlane(-direction, Vector3.forward);
-        turret.transform.rotation = Quaternion.FromToRotation(Vector3.right, direction2D);
+        if (shouldRotate)
+            sprite.transform.Rotate(new Vector3(0, 0, 1), rotationSpeed * Time.deltaTime);
+        else if (!hasPlayedSpawnTaunt) {
+            StartCoroutine(spawnTaunt());
+            hasPlayedSpawnTaunt = true;
+        }
+        if (shouldPointTurret) {
+            direction = player.transform.position - transform.position;
+            direction2D = Vector3.ProjectOnPlane(-direction, Vector3.forward);
+            turret.transform.rotation = Quaternion.FromToRotation(Vector3.right, direction2D);
+        }
 
 
         if (Input.GetKeyDown(KeyCode.Semicolon)) {
             resetWeakpoints();
             enableWeakpoints(Random.Range(3, 5));
         }
-        if (Input.GetKeyDown(KeyCode.M)) {
-            StartCoroutine(die());
-        }
+
+        if (curActionTime > 0f)
+            curActionTime = curActionTime - (Time.deltaTime);
+
     }
 
+    private IEnumerator spawnTaunt() {                       //from PlayerController hitAnimation
+        curActionTime = 4;
+        while (curActionTime > 0) {
+            sprite.transform.Rotate(new Vector3(0, 0, 360 * Time.deltaTime));
+            turret.transform.Rotate(new Vector3(0, 0, -720 * Time.deltaTime));
+            yield return new WaitForEndOfFrame();
+        }
+        shouldRotate = shouldPointTurret = true;
+        StartCoroutine(moveToAttackingPhase());
+    }
+
+
+
+    //i decided to hard code a bunch of stuff in here
+    private IEnumerator moveToAttackingPhase() {
+        //pick a side
+        Vector3 pos = transform.position;
+        if (Random.Range(1, 3) == 1)  //this is weird but 1 = left, 2 = right (the 3 is exclusive)
+            pos = new Vector3(-53, 0);
+        else
+            pos = new Vector3(53, 0);
+
+        //move to pos
+
+        shouldPointTurret = false;
+        float t = 0;
+        float duration = 1.2f;
+        Vector3 startPosition = transform.position;
+
+        while (t < duration) {
+            transform.position = Vector3.Lerp(startPosition, pos, t / duration);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = pos;
+
+        StartCoroutine(attackingPhase());
+
+    }
+
+    private IEnumerator attackingPhase() {
+        //determine where turret should point
+        Quaternion turretDir = Quaternion.identity;
+        if (transform.position.x > 0)
+            turretDir = Quaternion.Euler(0, 0, 180);
+        else
+            turretDir = Quaternion.Euler(0, 0, -180);
+
+
+        //stick to player Y
+
+
+        Vector3 target = transform.position;
+        Vector3 smoothPosition = transform.position;
+        float followSpeed = 2f;
+        float delay = 0.5f;
+
+        curActionTime = 15f;
+        while (curActionTime > 0) {
+            StartCoroutine(lockTurret(turretDir));
+            StartCoroutine(shoot(turretDir));
+            if (player.transform.position.y < vMovementBound && player.transform.position.y > -vMovementBound)
+                target = new Vector3(transform.position.x, player.transform.position.y, 0);
+            else if (player.transform.position.y > vMovementBound)
+                target = new Vector3(transform.position.x, vMovementBound, 0);
+            else if (player.transform.position.y < -vMovementBound)
+                target = new Vector3(transform.position.x, -vMovementBound, 0);
+            smoothPosition = Vector3.Lerp(smoothPosition, target, followSpeed * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, smoothPosition, delay);
+            yield return null;
+        }
+
+    }
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Player"))
             player.GetComponent<PlayerController>().hitPlayer();
 
+    }
+
+    private IEnumerator lockTurret(Quaternion direction) {
+        turret.transform.rotation = direction;
+        transform.Rotate(new Vector3(0, 0, 1), -rotationSpeed * Time.deltaTime);   //todo: smoothly rotate the turret instead of snapping it into place here
+        yield return new WaitForEndOfFrame();
+    }
+
+    private IEnumerator shoot(Quaternion direction) {
+        yield return new WaitForSeconds(1);
+        Instantiate(projectile, transform.position, direction).initialize();  //todo: shoot multiple projectiles out
+                                                                              //todo: add bosses into collision logic
+                                                                              //todo: update projectile script more (again)
     }
 
     private void resetWeakpoints() {
